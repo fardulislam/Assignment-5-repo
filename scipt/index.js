122db32e8bfea9510f8a03c7538758b94b9c654c
fardulislam <ifardin831@gmail.com>
2025-11-09 12:13:56 +0600

    add new function in js

--- scipt/index.js
+++ scipt/index.js
@@ -22,6 +22,57 @@ loveicon.forEach((btn)=>{
   })
 })
 
+let coin =100;
+
+const callbtn = document.getElementsByClassName('callbtn');
+for(let callbutton of callbtn){
+  callbutton.addEventListener('click',function(){
+    // product title //
+    const cardtitle = callbutton.parentNode.parentNode.children[1].innerText;
+    console.log(cardtitle)
+
+    // product title paragraph //
+    const cardparagraph =callbutton.parentNode.parentNode.children[3].innerText;
+    console.log(cardparagraph);
+
+    // navber coin btn //
+      
+    const navcoin =document.getElementById('coin');
+    console.log(navcoin)
+    if(coin>20){
+      coin-=20;
+      navcoin.textContent=coin;
+    }
+    else{
+      coin=0;
+      navcoin.textContent=coin;
+      alert('you have not enaf coin')
+      return;
+    }
+
+     const now = new Date();
+    const timeString = now.toLocaleTimeString();
+
+    const cardcontainer = document.getElementById('card-container')
+    const newcard = document.createElement('div')
+    newcard.innerHTML=`
+    <div class="p-2 flex justify-between items-center rounded-sm bg-gray-100 ">
+              <div>
+                <h1 class="text-sm font-bold">${cardtitle}</h1>
+              <h3 class='text-sm'>${cardparagraph} </h3>
+              </div>
+              <div>
+                <h3 class='text-sm'>${timeString}</h3>
+              </div>
+            </div>
+    `
+    cardcontainer.append(newcard)
+  })
+}
+
+
+
+
 
 
    
